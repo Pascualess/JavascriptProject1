50fe0eaaed5734555fedbd8e9e95effed31ddfb4
Jacob Pascua <pascuajakecreed@gmail.com>
2022-11-09 21:36:09 -0500

    test

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test")
+let firstForm = document.querySelector(`#formContainer`);
+
+firstForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = FormData(firstForm);
+  let fistdiv = document.querySelector(`firstPage`);
+
+  //display weekly budget
+  //display weekly budget - cost
+});
