937a3d816337d93157c0a0fbace6b7b62487659d
Jacob Pascua <pascuajakecreed@gmail.com>
2022-11-10 20:03:48 -0500

    finished 2nd form listern and part of the function that gets total cost

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 let firstForm = document.querySelector(`#formContainer`);
-let cost = 500; //this will target the array (of expenses) so we can select total cost
+let cost =  //this will target the array (of expenses) so we can select total cost
+let remainingIncome = income - cost;
+let expensesForm = document.querySelector(`placeholder for form that has cost`)
+let costArray = [{
+  numberCost: 50, typeCost: `bill`},
+  {numberCost: 50, typeCost: `bill`},
+  {numberCost: 50, typeCost: `bill`},
+]
 
 firstForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let data = FormData(firstForm);
-  let fistdiv = document.querySelector(`firstPage`);
 
-  let income = 2000; //data.get("inputContainerName");
+  let income = data.get("inputContainerName");
 
   let remainingIncome = income - cost;
+
   let testDiv = document.querySelector("#test");
+
   let pastElement = document.createElement("p");
   pastElement.innerText = `${remainingIncome}`;
   testDiv.appendChild(pastElement);
   console.log(testDiv);
 });
+
+expensesForm.addEventListener(`submit`,(e) => {
+  e.preventDefault();
+  let data = FormData(expensesForm)
+
+  let numberCost = data.get(`number for cost`)
+  let typeCost = data.get(`type of cost selected with drop down`)
+  let newCost = {
+    numberCost: numberCost,
+    typeCost: typeCost
+  }
+  costArray.push(newCost)
+})
+
+function totalSumOfNumberCost(costArray) {
+  let totalSum
+  for (i of costArray){
+    totalSum += i.numberCost
+  }
+}
