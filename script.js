567b44cacd485157437d9511f487a2cfdb6a1b95
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2022-11-10 21:10:10 -0500

    update html 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let firstForm = document.querySelector(`#formContainer`);
+// // let firstForm = document.querySelector(`#formContainer`);
 
-firstForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let data = FormData(firstForm);
-  let fistdiv = document.querySelector(`firstPage`);
+// // firstForm.addEventListener("submit", (e) => {
+// //   e.preventDefault();
+// //   let data = FormData(firstForm);
+// //   let fistdiv = document.querySelector(`firstPage`);
 
-  //display weekly budget
-  //display weekly budget - cost
-});
+//   //display weekly budget
+//   //display weekly budget - cost
+// });
