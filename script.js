724343dd0dcb6db8f54d2c9f284615d563bf0782
Jacob Pascua <pascuajakecreed@gmail.com>
2022-11-15 15:36:52 -0500

    cleaned up script and some displays

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ firstForm.addEventListener("submit", displayIncome);
 function displayIncome(e) {
   e.preventDefault();
   let data = new FormData(firstForm);
-
   let income = data.get("expensesNumber");
-
   let remainingIncome = +income - +totalSumOfNumberCost(costArray);
-
   let testDiv = document.querySelector("#incomeTotal");
-
   let pastElement = document.createElement("p");
-  pastElement.innerText = `${remainingIncome}`;
+
+  pastElement.innerText = `Weekly Budget Remaining: $${remainingIncome}`;
   testDiv.innerHTML = "";
   testDiv.appendChild(pastElement);
 }
@@ -26,37 +23,33 @@ function displayIncome(e) {
 expensesForm.addEventListener(`submit`, (e) => {
   e.preventDefault();
   let clear = document.querySelector(`#filterDisplay`);
-  let data2 = new FormData(firstForm); //gets the income input
-  let income = data2.get("expensesNumber"); //sets variable to that input
-  // let remainingIncome = +income - +totalSumOfNumberCost(costArray);// variable is equal to the input - the array (the array is empty)
-  let data = new FormData(expensesForm); // gets how much the expense costs (number)
-  let typeCost = document.querySelector(`#expensesType`); // gets the type of expense
-
-  let numberCost = data.get(`expenseNumber`); // sets input equal to variable
-  // let typeCost = data.get(`type`)
-
+  let data2 = new FormData(firstForm);
+  let income = data2.get("expensesNumber");
+  let data = new FormData(expensesForm);
+  let typeCost = document.querySelector(`#expensesType`);
+  let numberCost = data.get(`expenseNumber`);
+  let testDiv = document.querySelector("#incomeTotal");
+  let pastElement = document.createElement("p");
   let newCost = {
     numberCost: +numberCost,
     typeCost: typeCost.value,
-  }; // object with both inputs that can be pushed to the array
-  costArray.push(newCost); // the push
+  };
+
+  costArray.push(newCost);
+
   if (income - totalSumOfNumberCost(costArray) < 0) {
     alert(`Insufficant funds.`);
     costArray.pop();
     return;
   }
 
-  // totalSumOfNumberCost(costArray);// finds total cost on the array? (not sure why i need this it returns the sum)
   displayExpenseList(); // goes through the list and prints them to a <ul>
 
-  let testDiv = document.querySelector("#incomeTotal"); // targets where im putting the remain income
-
-  let pastElement = document.createElement("p"); //variable to make the <p>
-  pastElement.innerText = `${+income - totalSumOfNumberCost(costArray)}`; //prints the remainingIncome
-  testDiv.innerHTML = ""; //emptys the list to prevent duping
-  testDiv.appendChild(pastElement); //puts the <p> on the DOM
+  pastElement.innerText = `Weekly Budget Remaining: $${+income - totalSumOfNumberCost(costArray)}`;
+  testDiv.innerHTML = "";
+  testDiv.appendChild(pastElement);
   console.log(totalSumOfNumberCost(costArray));
-  clear.innerHTML = ``;
+  clear.innerHTML = ``; //removes the filter array
 });
 
 // this finds the sum of the cost of ever item in the array
@@ -87,52 +80,30 @@ filter.addEventListener(`click`, setFilter);
 
 function setFilter() {
   let filterSetting = document.querySelector(`#expensesFilter`);
-
   let filterDisplay = document.querySelector(`#filterDisplay`);
-  if (filterSetting.value === `entertainment`) {
-    let result = costArray.filter((i) => i.typeCost === `entertainment`);
-    filterDisplay.innerHTML = ``;
-    let sum = 0;
-    for (i of result) {
-      sum += i.numberCost;
-      let elem = document.createElement("li");
-      elem.innerText = `amount: $${i.numberCost} | expense type: ${i.typeCost}`;
-      filterDisplay.appendChild(elem);
-    }
+  if (filterSetting.value === `all`) {
+    filterDisplay.innerText = ``
     let elemSum = document.createElement(`p`);
-    elemSum.innerText = `The total cost of entertainment: $${sum}`;
+    elemSum.innerText = `The total cost of all expenses: $${totalSumOfNumberCost(costArray)}`;
     filterDisplay.appendChild(elemSum);
+  }
+  if (filterSetting.value === `entertainment`) {
+    forFilterDisplay(`entertainment`, filterDisplay, filterSetting);
   }
   if (filterSetting.value === `food`) {
-    let result = costArray.filter((i) => i.typeCost === `food`);
-    filterDisplay.innerHTML = ``;
-    let sum = 0;
-    for (i of result) {
-      sum += i.numberCost;
-      let elem = document.createElement("li");
-      elem.innerText = `amount: $${i.numberCost} | expense type: ${i.typeCost}`;
-      filterDisplay.appendChild(elem);
-    }
-    let elemSum = document.createElement(`p`);
-    elemSum.innerText = `The total cost of food: $${sum}`;
-    filterDisplay.appendChild(elemSum);
+    forFilterDisplay(`food`, filterDisplay, filterSetting);
   }
   if (filterSetting.value === `clothing`) {
-    let result = costArray.filter((i) => i.typeCost === `clothing`);
-    filterDisplay.innerHTML = ``;
-    let sum = 0;
-    for (i of result) {
-      sum += i.numberCost;
-      let elem = document.createElement("li");
-      elem.innerText = `amount: $${i.numberCost} | expense type: ${i.typeCost}`;
-      filterDisplay.appendChild(elem);
-    }
-    let elemSum = document.createElement(`p`);
-    elemSum.innerText = `The total cost of clothing: $${sum}`;
-    filterDisplay.appendChild(elemSum);
+    forFilterDisplay(`clothing`, filterDisplay, filterSetting);
   }
   if (filterSetting.value === `bills`) {
-    let result = costArray.filter((i) => i.typeCost === `bills`);
+    forFilterDisplay(`bills`, filterDisplay, filterSetting);
+  }
+}
+
+function forFilterDisplay(expenseType, filterDisplay, filterSetting) {
+  if (filterSetting.value === `${expenseType}`) {
+    let result = costArray.filter((i) => i.typeCost === `${expenseType}`);
     filterDisplay.innerHTML = ``;
     let sum = 0;
     for (i of result) {
@@ -142,7 +113,7 @@ function setFilter() {
       filterDisplay.appendChild(elem);
     }
     let elemSum = document.createElement(`p`);
-    elemSum.innerText = `The total cost of bills: $${sum}`;
+    elemSum.innerText = `The total cost of ${expenseType}: $${sum}`;
     filterDisplay.appendChild(elemSum);
   }
 }
